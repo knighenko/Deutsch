b371461890e75ea9e2b7cf8283d9d03ebbc27a93
Maksym Knyzhenko <knighenko@gmail.com>
2020-11-12 14:25:00 +0200

    Add Authentication

---
+++ app/src/main/java/com/knighenko/deutsch/Authentication.java
@@ -0,0 +1,14 @@
+package com.knighenko.deutsch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Authentication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
