745864b19ad61d0d9d07a754b1ced1d11f7d537e
Maksym Knyzhenko <knighenko@gmail.com>
2020-11-13 20:34:04 +0200

    Add LessonsActivity

--- app/src/main/java/com/knighenko/deutsch/AuthenticationActivity.java
+++ app/src/main/java/com/knighenko/deutsch/AuthenticationActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Authentication extends AppCompatActivity {
+public class AuthenticationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
