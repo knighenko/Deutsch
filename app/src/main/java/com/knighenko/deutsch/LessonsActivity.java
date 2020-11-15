26a04bdae71e16b9d19f45aa612aa979b92c3d4c
Maksym Knyzhenko <knighenko@gmail.com>
2020-11-15 22:04:33 +0200

    Change activity_lessons.xml

--- app/src/main/java/com/knighenko/deutsch/LessonsActivity.java
+++ app/src/main/java/com/knighenko/deutsch/LessonsActivity.java
@@ -17,18 +17,6 @@ public class LessonsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lessons);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
-        toolBarLayout.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
